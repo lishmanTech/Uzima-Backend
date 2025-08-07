2f5d553ea538c57cd988a7dc57ce7bf7dbc86ffa
KAMALDEEN333 <kamalaliyu503@gmail.com>
2025-08-07 13:29:36 +0000

    Implemented recommendation logic

--- service/recommendationService.js
+++ service/recommendationService.js
@@ -2,23 +2,34 @@
 const UserReadHistory = require('../models/UserReadHistory');
 const Article = require('../models/Article');
 
+
 async function getRecommendedArticles(userId, limit = 10, skip = 0) {
   // Get articles user has read
   const readHistory = await UserReadHistory.find({ userId }).select('articleId');
   const readArticleIds = readHistory.map(r => r.articleId);
 
-  // Get topics from read articles
+  // Get topics and tags from read articles
   const readArticles = await Article.find({ _id: { $in: readArticleIds } });
   const topics = [...new Set(readArticles.flatMap(a => a.topics))];
+  const tags = [...new Set(readArticles.flatMap(a => a.tags || []))];
 
-  // Recommend articles with similar topics, not already read
+  // Recommend articles with similar topics or tags, not already read
+  // Rank by engagement (likes, views, shares) and recency
   const recommendations = await Article.find({
-    topics: { $in: topics },
-    _id: { $nin: readArticleIds }
+    $and: [
+      { _id: { $nin: readArticleIds } },
+      {
+        $or: [
+          { topics: { $in: topics } },
+          { tags: { $in: tags } }
+        ]
+      }
+    ]
   })
-    .sort({ publishedAt: -1 })
+    .sort({ likes: -1, views: -1, shares: -1, publishedAt: -1 })
     .skip(skip)
-    .limit(limit);
+    .limit(limit)
+    .select('title summary url imageUrl author topics tags publishedAt likes views shares');
 
   return recommendations;
 }
